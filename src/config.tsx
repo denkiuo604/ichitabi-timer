8ea986779d80af441e61f9688c762a978bf3ace5
denkiuo604 <denkiuo604@gmail.com>
2023-02-16 08:27:34 +0900

    Modify src/config.tsx
    バグが発覚したため臨時対応
    設定値を別ファイルに移行

--- src/config.tsx
+++ src/config.tsx
@@ -6,8 +6,12 @@
 export const pubWeeks = [2, 3]
 export const pubDays = [4]
 
+// 公開時刻の設定
+export const pubHour = 18
+export const pubMinute = 0
+
 // お休み月の設定
 export const offMonth = 0
 
 // 臨時のスケジュール変更有無
-export const tempSchedule = false
+export const tempSchedule = true
