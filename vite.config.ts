d7dcd216cc9507eb00e9ffc9d7e7912cd4b9c5dd
denkiuo604 <denkiuo604@gmail.com>
2023-03-08 00:01:24 +0900

    Revert manifest.json

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: "/ichitabi-timer/",
-  assetsInclude: ["robots.txt"],
+  assetsInclude: ["robots.txt", "manifest.json"],
 })
