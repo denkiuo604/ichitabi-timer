fb01a8f7577b8d9cbd6151ffc8337b55dc740ae0
denkiuo604 <denkiuo604@gmail.com>
2023-05-12 23:00:01 +0900

    Modify src/config.tsx
    5月第2週 公開日時変更対応 を戻した

--- src/config.tsx
+++ src/config.tsx
@@ -7,10 +7,10 @@ export const days = ['日', '月', '火', '水', '木', '金', '土']
 
 // 公開週・曜日の設定
 export const pubWeeks = [2, 4]
-export const pubDays = [4, 5]
+export const pubDays = [4]
 
 // 公開時刻の設定
-export const pubHour = 23
+export const pubHour = 18
 export const pubMinute = 0
 
 // お休み月の設定
