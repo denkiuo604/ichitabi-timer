b62a2309988d47b3ae9f5107811410609e0fc529
denkiuo604 <denkiuo604@gmail.com>
2023-05-26 08:19:58 +0900

    Modify src/config.tsx
    5月第4週 公開日変更対応
    https://twitter.com/shikuni_ichiro/status/1661742692408397825

--- src/config.tsx
+++ src/config.tsx
@@ -7,14 +7,14 @@ export const days = ['日', '月', '火', '水', '木', '金', '土']
 
 // 公開週・曜日の設定
 export const pubWeeks = [2, 4]
-export const pubDays = [4]
+export const pubDays = [4, 5]
 
 // 公開時刻の設定
-export const pubHour = 18
+export const pubHour = 23
 export const pubMinute = 0
 
 // お休み月の設定
 export const offMonths = [4, 8, 12]
 
 // 臨時のスケジュール変更有無
-export const tempSchedule = true
+export const tempSchedule = false
