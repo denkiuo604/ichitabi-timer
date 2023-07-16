18369d0d77635c0079f5f2fd08d6227b205ccf52
denkiuo604 <denkiuo604@gmail.com>
2023-07-16 19:51:38 +0900

    Modify src/config.tsx
    7月第2週 公開日変更対応 を戻した
    https://twitter.com/shikuni_ichiro/status/1679686300402872321

--- src/config.tsx
+++ src/config.tsx
@@ -6,11 +6,11 @@
 export const days = ['日', '月', '火', '水', '木', '金', '土']
 
 // 公開週・曜日の設定
-export const pubWeeks = [3, 4]
-export const pubDays = [0, 4]
+export const pubWeeks = [2, 4]
+export const pubDays = [4]
 
 // 公開時刻の設定
-export const pubHour = 20
+export const pubHour = 23
 export const pubMinute = 0
 
 // お休み月の設定
