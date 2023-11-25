69a609a15e87d9a5e761531e505e51f132556e2c
denkiuo604 <denkiuo604@gmail.com>
2023-11-25 21:33:25 +0900

    Modify vite.config.ts
    デバッグ実行時に自動でブラウザが立ち上がるようにした

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    open: true,
+  },
   plugins: [react()],
   base: "/ichitabi-timer/",
   assetsInclude: ["robots.txt", "manifest.json"],
