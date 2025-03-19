c8832327d6cbfd74e36c3d531b4e8c8261355533
denkiuo604 <denkiuo604@gmail.com>
2025-03-19 23:37:04 +0900

    Modify src/config.tsx
    3月第4週 公開日変更対応
    https://www.youtube.com/watch?v=PSXsxNuM5Ys

--- src/config.tsx
+++ src/config.tsx
@@ -7,7 +7,7 @@ export const days = ['日', '月', '火', '水', '木', '金', '土']
 
 // 公開週・曜日の設定
 export const pubWeeks = [4]
-export const pubDays = [4]
+export const pubDays = [0]
 
 // 公開時刻の設定
 export const pubHour = 23
@@ -17,4 +17,4 @@ export const pubMinute = 0
 export const offMonths = [4, 8, 12]
 
 // 臨時のスケジュール変更有無
-export const tempSchedule = true
+export const tempSchedule = false
