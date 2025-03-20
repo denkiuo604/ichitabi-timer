16620633f62274ebe4837c369f3bfe4433a3f6b1
denkiuo604 <denkiuo604@gmail.com>
2025-03-20 22:46:32 +0900

    Modify src/config.tsx
    3月第4週 公開時刻変更対応
    https://x.com/shikuni_ichiro/status/1902673368807252029

--- src/config.tsx
+++ src/config.tsx
@@ -10,8 +10,8 @@ export const pubWeeks = [4]
 export const pubDays = [0]
 
 // 公開時刻の設定
-export const pubHour = 23
-export const pubMinute = 0
+export const pubHour = 19
+export const pubMinute = 30
 
 // お休み月の設定
 export const offMonths = [4, 8, 12]
